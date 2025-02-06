bb634c98cd1070b6e9a7b3efdda8b5db362714ef
Lucas Santos <lucasa95santos@gmail.com>
2025-02-05 21:36:31 -0300

    chore: initial config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const { compilerOptions } = require('./tsconfig.paths.json');
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['node_modules', '<rootDir>'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  testTimeout: 60000,
+};
